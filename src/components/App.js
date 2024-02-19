438cf7f773d21dd1c1333498bc324da67fb88fa2
Claudia00022 <klaudiaforysiak@gmail.com>
2024-02-19 18:10:22 +0000

    stylikng app

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App(){
-  return <h1>Heloo</h1>
+  return <div>
+  <h1 className="heading">Birthday of your soulmate</h1>
+  <p className="plain">Whats yours day of birth?</p>
+  <input></input>
+  <input></input>
+  <input></input>
+  <button>check</button>
+  <div>
+    <img></img>
+  </div>
+  <p className="plain">Your soulmate birthday is:</p>
+  <div></div>
+  </div>
 };
 
 
