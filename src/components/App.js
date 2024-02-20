d62afeacd6083433584ef40a7a63fbb4cb14125b
Claudia00022 <klaudiaforysiak@gmail.com>
2024-02-20 18:00:39 +0000

    styling

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App(){
   return <div>
+  
   <h1 className="heading">Birthday of your soulmate</h1>
   <p className="plain">Whats yours day of birth?</p>
-  <input></input>
-  <input></input>
-  <input></input>
-  <button>check</button>
-  <div>
-    <img></img>
+  <div className="container  w-75 m-auto">
+  <div className="row justify-content-center">
+  <input className = "col-2 me-2"></input>
+  <input className="col-4 me-2"></input>
+  <input className="col-2"></input>
+  </div>
+  </div>
+  <button className="check d-block m-auto mt-5 btn ">check</button>
+  <div className="couple-image m-auto">
+    <img className="img-fluid w-100" src="/images/para.png" alt="para"></img>
+  </div>
+  <div className="eclipse-left h-100">
+  <img className="img-fluid w-100" src="/images/eclipse-left.png"></img>
+  </div>
+  <div className="letter">
+  <img className="img-fluid w-100" src="/images/letter.png"></img>
   </div>
   <p className="plain">Your soulmate birthday is:</p>
   <div></div>
