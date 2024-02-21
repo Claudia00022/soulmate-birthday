d2d069e45c40bdf370f4e02b64335d357e11dc98
Claudia00022 <klaudiaforysiak@gmail.com>
2024-02-21 18:12:41 +0000

    random date appear after click dateof birth

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App(){
+  const[day, setDay] = useState("");
+  const[month,setMonth] = useState("");
+  const[year, setYear] = useState("");
+
+  const[randomD, setRandomD] = useState("");
+  const[randomM, setRandomM] = useState("");
+
+
+
+  const days = Array.from({length: 31}, (_, index) => index +1);
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November","December" ];
+  const years = Array.from({length:100}, (_, index) => 2022 - index);
+
+  const randomDay = Math.floor((Math.random() * 30)+1);
+  const randomMonth = months[Math.floor(Math.random() * 12)];
+
+  function handleClick(){
+    setDay("");
+    setMonth("");
+    setYear("");
+
+    setRandomD(randomDay);
+    setRandomM(randomMonth);
+
+
+  
+  }
   return <div>
   
   <h1 className="heading">Birthday of your soulmate</h1>
   <p className="plain">Whats yours day of birth?</p>
   <div className="container  w-75 m-auto">
   <div className="row justify-content-center">
-  <input className = "col-2 me-2"></input>
+  {/* <input className = "col-2 me-2"></input>
   <input className="col-4 me-2"></input>
-  <input className="col-2"></input>
+  <input className="col-2"></input> */}
+
+
+      <select className="col-2 me-2"  value={day} onChange={(e) => setDay(e.target.value)}>
+        <option value="">Select Day</option>
+        {days.map((d) => (
+          <option key={d} value={d}>{d}</option>
+        ))}
+      </select>
+      
+
+    
+      <select className="col-4 me-2"value={month} onChange={(e) => setMonth(e.target.value)}>
+        <option value="">Select Month</option>
+        {months.map((m, index) => (
+          <option key={index + 1} value={index + 1}>{m}</option>
+        ))}
+      </select>
+
+   
+      <select className="col-2 me-2" value={year} onChange={(e) => setYear(e.target.value)}>
+        <option value="">Select Year</option>
+        {years.map((y) => (
+          <option key={y} value={y}>{y}</option>
+        ))}
+      </select>
+
   </div>
   </div>
-  <button className="check d-block m-auto mt-5 btn ">check</button>
+  <button className="check d-block m-auto mt-5 btn "  onClick = {handleClick}>check</button>
   <div className="couple-image m-auto">
     <img className="img-fluid w-100" src="/images/para.png" alt="para"></img>
   </div>
@@ -24,7 +77,9 @@ function App(){
   <img className="img-fluid w-100" src="/images/letter.png"></img>
   </div>
   <p className="plain">Your soulmate birthday is:</p>
-  <div></div>
+  <div className="circle"></div>
+  <div className="circle-blue"></div>
+  <h1 className="randomText border">{randomD} {randomM} </h1>
   </div>
 };
 
